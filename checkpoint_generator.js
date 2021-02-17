8c6b3d116417fc02d16c40bdda328e3678e0c5ab
Tobias aka. TheDevMinerTV <tobigames200@gmail.com>
2021-02-17 18:22:07 +0100

    Rework checkpoint generator

--- checkpoint_generator.js
+++ checkpoint_generator.js
@@ -1,5 +1,5 @@
-const fs = require('fs'),
-      { TurtleCoind } = require('turtlecoin-rpc');
+const fs = require('fs');
+const { TurtleCoind } = require('turtlecoin-rpc');
 
 const daemon = new TurtleCoind({
 	host: '127.0.0.1', // ip address or hostname of the Telluriumd host
@@ -15,9 +15,9 @@ class Checkpoint {
 	}
 }
 
-var checkpoints = [];
-
 daemon.blockCount().then(async (height) => {
+	const checkpoints = [];
+
 	console.log("Got blockheight " + height + "!");
 
 	for (let i = 1; i < height; i++) {
@@ -30,29 +30,9 @@ daemon.blockCount().then(async (height) => {
 
 	console.log('Got', checkpoints.length, 'checkpoints from 0 to', checkpoints[checkpoints.length - 1].height, 'blocks');
 
-	let csv = ''
+	const csv = checkpoints.map((c) => `${checkpoint.height},${checkpoint.hash}`).join('\n'));
 
-	for (const checkpoint of checkpoints) {
-		csv += `${checkpoint.height},${checkpoint.hash}\n`
-	}
-
-	const buffer = Buffer.from(csv, 'ascii');
-
-	fs.open('checkpoints.csv', 'w', (err, fd) => {
-		if (err) throw err
-
-		console.log('Opened checkpoints.csv!');
-
-		fs.write(fd, buffer, 0, buffer.byteLength, 0, (err, bytes) => {
-			if(err) throw err;
-
-			console.log('Wrote', Math.floor(bytes / 1024), ' kilobytes');
-
-			fs.close(fd, (err) => {
-				console.log('Closed checkpoints.csv!');
-			});
-		});
-	})
+	fs.writeFileSync('checkpoints.csv', csv);
 })
 .catch((error) => {
 	throw new Error(`An error occurred whilst getting height of the blockchain : ${error}`);
